ffc7919d85a9889f33e679653bb527c66114f466
Ruben Urrego <109879800+RubenU2002@users.noreply.github.com>
2025-06-16 09:54:01 -0500

    Add Swagger documentation (#2)

--- src/main/java/org/sci/serviciogestioncalificaciones/ServicioGestionCalificacionesApplication.java
+++ src/main/java/org/sci/serviciogestioncalificaciones/ServicioGestionCalificacionesApplication.java
@@ -2,7 +2,11 @@ package org.sci.serviciogestioncalificaciones;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
+@OpenAPIDefinition(info = @Info(title = "API Gestion Calificaciones", version = "1.0",
+        description = "Documentacion de la API"))
 @SpringBootApplication
 public class ServicioGestionCalificacionesApplication {
 
--- src/main/java/org/sci/serviciogestioncalificaciones/controller/ApiController.java
+++ src/main/java/org/sci/serviciogestioncalificaciones/controller/ApiController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
 
 @RestController
 @RequestMapping("/api")
@@ -31,31 +32,37 @@ public class ApiController {
     }
 
     @PostMapping("/students")
+    @Operation(summary = "Crear estudiante")
     public Student createStudent(@RequestBody Student student) {
         return studentService.save(student);
     }
 
     @GetMapping("/students")
+    @Operation(summary = "Listar estudiantes")
     public List<Student> getStudents() {
         return studentService.findAll();
     }
 
     @PostMapping("/courses")
+    @Operation(summary = "Crear curso")
     public Course createCourse(@RequestBody Course course) {
         return courseService.save(course);
     }
 
     @GetMapping("/courses")
+    @Operation(summary = "Listar cursos")
     public List<Course> getCourses() {
         return courseService.findAll();
     }
 
     @PostMapping("/enrollments")
+    @Operation(summary = "Matricular estudiante")
     public Enrollment createEnrollment(@RequestBody Enrollment enrollment) {
         return enrollmentService.save(enrollment);
     }
 
     @GetMapping("/courses/{id}/enrollments")
+    @Operation(summary = "Consultar notas por curso")
     public List<Enrollment> getEnrollmentsByCourse(@PathVariable Long id) {
         return enrollmentService.findByCourseId(id);
     }
